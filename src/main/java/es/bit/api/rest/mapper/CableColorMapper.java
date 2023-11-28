d57d0a8fd204a3b80b9c338fa748f3aa6786ef67
Blas Fernández G. <batoibfg2001@gmail.com>
2023-11-28 17:20:50 +0100

    [ADD] Se ha añadido el módulo 'CableColors' #1 - Se ha incluido una paginación y la documentación del módulo con OpenAPI.

---
+++ src/main/java/es/bit/api/rest/mapper/CableColorMapper.java
@@ -0,0 +1,53 @@
+package es.bit.api.rest.mapper;
+
+import es.bit.api.persistence.model.CableColor;
+import es.bit.api.rest.dto.CableColorDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class CableColorMapper {
+    public static CableColorDTO toDTO(CableColor cableColor) {
+        CableColorDTO cableColorDTO = new CableColorDTO();
+
+        if (cableColor != null) {
+            cableColorDTO.setId(cableColor.getId());
+            cableColorDTO.setName(cableColor.getName());
+        }
+
+        return cableColorDTO;
+    }
+
+
+    public static CableColorDTO toDTO(Optional<CableColor> cableColorOptional) {
+        return cableColorOptional.map(CableColorMapper::toDTO).orElse(null);
+    }
+
+
+    public static List<CableColorDTO> toDTO(List<CableColor> cableColors) {
+        List<CableColorDTO> cableColorsDTO = new ArrayList<>();
+
+        if (cableColors == null) {
+            return cableColorsDTO;
+        }
+
+        for (CableColor cableColor : cableColors) {
+            cableColorsDTO.add(CableColorMapper.toDTO(cableColor));
+        }
+
+        return cableColorsDTO;
+    }
+
+
+    public static CableColor toBD(CableColorDTO cableColorDTO) {
+        CableColor cableColor = new CableColor();
+
+        if (cableColorDTO != null) {
+            cableColor.setId(cableColorDTO.getId());
+            cableColor.setName(cableColorDTO.getName());
+        }
+
+        return cableColor;
+    }
+}
---
+++ src/main/java/es/bit/api/utils/PagedResponse.java
@@ -0,0 +1,75 @@
+package es.bit.api.utils;
+
+import java.util.List;
+
+/**
+ *  Paged response containing a list of items, paging information and paging statistics.
+ *
+ * @param <T> Type of elements contained in the paged response.
+ */
+public class PagedResponse<T> {
+    private List<T> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+
+
+    /**
+     * PagedResponse constructor.
+     *
+     * @param content List of elements present in the current page.
+     * @param page Number of the actual page.
+     * @param size Size of the actual page.
+     * @param totalElements Total number of elements on all pages.
+     * @param totalPages Total number of pages available.
+     */
+    public PagedResponse(List<T> content, int page, int size, long totalElements, int totalPages) {
+        this.content = content;
+        this.page = page;
+        this.size = size;
+        this.totalElements = totalElements;
+        this.totalPages = totalPages;
+    }
+
+
+    public List<T> getContent() {
+        return content;
+    }
+
+    public void setContent(List<T> content) {
+        this.content = content;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public long getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(long totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+}

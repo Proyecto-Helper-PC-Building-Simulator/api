37e63d7a54467a2f913f24e521d167c13425b6a2
Blas Fernández G. <batoibfg2001@gmail.com>
2024-12-24 01:47:53 +0100

    feat: add method that generates a list of integers between 2 numbers

--- src/main/java/es/bit/api/persistence/repository/jpa/components/ICpuJpaRepository.java
+++ src/main/java/es/bit/api/persistence/repository/jpa/components/ICpuJpaRepository.java
@@ -2,6 +2,11 @@ package es.bit.api.persistence.repository.jpa.components;
 
 import es.bit.api.persistence.model.components.Cpu;
 import es.bit.api.persistence.repository.jpa.IGenericJpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Map;
 
 public interface ICpuJpaRepository extends IGenericJpaRepository<Cpu, Integer> {
+    @Query("SELECT MIN(c.wattage) as minWattage, MAX(c.wattage) as maxWattage FROM Cpu c")
+    Map<String, Integer> findWattageRange();
 }
--- src/main/java/es/bit/api/rest/service/FilterService.java
+++ src/main/java/es/bit/api/rest/service/FilterService.java
@@ -1,9 +1,11 @@
 package es.bit.api.rest.service;
 
 import es.bit.api.persistence.repository.jpa.components.IComponentJpaRepository;
+import es.bit.api.persistence.repository.jpa.components.ICpuJpaRepository;
 import es.bit.api.persistence.repository.jpa.components.attributes.IComponentTypeJPARepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,12 +13,14 @@ import java.util.Map;
 @Service
 public class FilterService {
     private final IComponentJpaRepository componentRepository;
+    private final ICpuJpaRepository cpuRepository;
     private final IComponentTypeJPARepository componentTypeRepository;
 
 
-    public FilterService(IComponentJpaRepository componentRepository, IComponentTypeJPARepository componentTypeRepository) {
+    public FilterService(IComponentJpaRepository componentRepository, IComponentTypeJPARepository componentTypeRepository, ICpuJpaRepository cpuRepository) {
         this.componentRepository = componentRepository;
         this.componentTypeRepository = componentTypeRepository;
+        this.cpuRepository = cpuRepository;
     }
 
 
@@ -33,6 +37,32 @@ public class FilterService {
         filters.put("manufacturers", manufacturers);
         filters.put("priceRange", priceRange);
 
+        switch (componentType.toLowerCase()) {
+            case "cpu":
+                Map<String, Integer> wattageRange = cpuRepository.findWattageRange();
+                List<Integer> wattageIntervals = generateDynamicRange(
+                        wattageRange.get("minWattage"),
+                        wattageRange.get("maxWattage")
+                );
+                filters.put("wattageIntervals", wattageIntervals);
+                break;
+        }
+
         return filters;
+    }
+
+
+    protected List<Integer> generateDynamicRange(int minValue, int maxValue) {
+        int interval = (maxValue <= 1000) ? 50 : 500;
+
+        int adjustedMin = (minValue / interval) * interval;
+        int adjustedMax = ((maxValue + interval - 1) / interval) * interval;
+
+        List<Integer> range = new ArrayList<>();
+        for (int i = adjustedMin; i <= adjustedMax; i += interval) {
+            range.add(i);
+        }
+
+        return range;
     }
 }

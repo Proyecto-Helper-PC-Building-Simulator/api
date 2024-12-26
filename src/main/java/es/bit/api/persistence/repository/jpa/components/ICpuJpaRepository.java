bac9aa4bdb1077f83d1edcf5a8df5223b838eca5
Blas Fernández G. <batoibfg2001@gmail.com>
2024-12-26 16:59:03 +0100

    feat: add methods that gets cpu filter values

--- src/main/java/es/bit/api/persistence/repository/jpa/components/ICpuJpaRepository.java
+++ src/main/java/es/bit/api/persistence/repository/jpa/components/ICpuJpaRepository.java
@@ -4,9 +4,22 @@ import es.bit.api.persistence.model.components.Cpu;
 import es.bit.api.persistence.repository.jpa.IGenericJpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Map;
 
 public interface ICpuJpaRepository extends IGenericJpaRepository<Cpu, Integer> {
     @Query("SELECT MIN(c.wattage) as minWattage, MAX(c.wattage) as maxWattage FROM Cpu c")
     Map<String, Integer> findWattageRange();
+
+    @Query("SELECT DISTINCT c.cores FROM Cpu c ORDER BY cores ASC")
+    List<Integer> findDistinctCores();
+
+    @Query("SELECT MIN(c.frequency) as minFrequency, MAX(c.frequency) as maxFrequency FROM Cpu c")
+    Map<String, Integer> findFrequencyRange();
+
+    @Query("SELECT DISTINCT c.cpuSocket.name FROM Cpu c")
+    List<String> findDistinctCpuSockets();
+
+    @Query("SELECT DISTINCT c.cpuSerie.name FROM Cpu c")
+    List<String> findDistinctCpuSeries();
 }
--- src/main/java/es/bit/api/rest/service/FilterService.java
+++ src/main/java/es/bit/api/rest/service/FilterService.java
@@ -30,21 +30,24 @@ public class FilterService {
             throw new IllegalArgumentException("Invalid component type: " + componentType);
         }
 
-        List<String> manufacturers = componentRepository.findDistinctManufacturersByType(componentType);
-        Map<String, Double> priceRange = componentRepository.findPriceRangeByType(componentType);
-
         Map<String, Object> filters = new HashMap<>();
-        filters.put("manufacturers", manufacturers);
-        filters.put("priceRange", priceRange);
+
+        filters.put("manufacturers", componentRepository.findDistinctManufacturersByType(componentType));
+        filters.put("priceRange", componentRepository.findPriceRangeByType(componentType));
 
         switch (componentType.toLowerCase()) {
             case "cpu":
                 Map<String, Integer> wattageRange = cpuRepository.findWattageRange();
-                List<Integer> wattageIntervals = generateDynamicRange(
-                        wattageRange.get("minWattage"),
-                        wattageRange.get("maxWattage")
-                );
+                List<Integer> wattageIntervals = generateDynamicRange(wattageRange.get("minWattage"), wattageRange.get("maxWattage"));
+
+                Map<String, Integer> frequencyRange = cpuRepository.findFrequencyRange();
+                List<Integer> frequencyIntervals = generateDynamicRange(frequencyRange.get("minFrequency"), frequencyRange.get("maxFrequency"));
+
                 filters.put("wattageIntervals", wattageIntervals);
+                filters.put("frequencyIntervals", frequencyIntervals);
+                filters.put("sockets", cpuRepository.findDistinctCpuSockets());
+                filters.put("series", cpuRepository.findDistinctCpuSeries());
+                filters.put("cores", cpuRepository.findDistinctCores());
                 break;
         }
 

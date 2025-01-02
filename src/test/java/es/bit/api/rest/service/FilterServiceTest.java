47ff1b3c09abe6ef3ccf79c4e1e987d832a28656
Blas Fernández G. <batoibfg2001@gmail.com>
2025-01-02 13:03:08 +0100

    fix: fix problem on FilterServiceTest

--- src/test/java/es/bit/api/rest/service/FilterServiceTest.java
+++ src/test/java/es/bit/api/rest/service/FilterServiceTest.java
@@ -34,10 +34,13 @@ public class FilterServiceTest {
         String componentType = "cpu";
         List<String> manufacturers = List.of("Intel", "AMD");
         List<Integer> wattageRangeResult = List.of(0, 50, 100, 150, 200, 250, 300);
+        List<Integer> frequencyRangeResult = List.of(2000, 2500, 3000, 3500, 4000, 4500, 5000);
         Map<String, Integer> wattageRange = Map.of("minWattage", 35, "maxWattage", 280);
+        Map<String, Integer> frequencyRange = Map.of("minFrequency", 2000, "maxFrequency", 5000);
         Map<String, Double> priceRange = Map.of("minPrice", 100.0, "maxPrice", 1200.0);
 
         Mockito.when(cpuRepository.findWattageRange()).thenReturn(wattageRange);
+        Mockito.when(cpuRepository.findFrequencyRange()).thenReturn(frequencyRange);
         Mockito.when(componentTypeRepository.existsByNameIdentifier(componentType)).thenReturn(true);
         Mockito.when(componentRepository.findDistinctManufacturersByType(componentType)).thenReturn(manufacturers);
         Mockito.when(componentRepository.findPriceRangeByType(componentType)).thenReturn(priceRange);
@@ -47,6 +50,7 @@ public class FilterServiceTest {
         Assertions.assertEquals(manufacturers, filters.get("manufacturers"));
         Assertions.assertEquals(priceRange, filters.get("priceRange"));
         Assertions.assertEquals(wattageRangeResult, filters.get("wattageIntervals"));
+        Assertions.assertEquals(frequencyRangeResult, filters.get("frequencyIntervals"));
     }
 
     @Test

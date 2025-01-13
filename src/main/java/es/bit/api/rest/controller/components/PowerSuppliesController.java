0eecefc4f1f514e85664cb29e3d5cf1cb0683749
Blas Fernández G. <batoibfg2001@gmail.com>
2025-01-13 13:40:50 +0100

    feat: add custom paged response with more information

--- src/main/java/es/bit/api/rest/controller/components/PowerSuppliesController.java
+++ src/main/java/es/bit/api/rest/controller/components/PowerSuppliesController.java
@@ -8,7 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +29,7 @@ public class PowerSuppliesController extends GenericController<PowerSupplyDTO, P
     @Operation(summary = "Get all power supplies paged")
     @ApiResponse(responseCode = "200", description = "Power supplies obtained correctly.")
     @ApiResponse(responseCode = "412", description = "Error getting the selected page.")
-    public Page<PowerSupplyDTO> findAll(
+    public ResponseEntity<?> findAll(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "50") int size,
             @RequestParam(defaultValue = "name") String sortBy,
